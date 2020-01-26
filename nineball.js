e1fa82d43d334a78ca18bb5e2f07f0c0b1e2eaac
Storm-Eagle20 <johnfausnaught@gmail.com>
2020-01-26 08:21:24 -0500

    Update nineball.js

--- nineball.js
+++ nineball.js
@@ -1,5 +1,5 @@
 const Eris = require("eris");
-const bot = new Eris("NTc0NTY5OTc0NDkzMDg1NzA2.Xi17_Q.NgI5PUtDosXaS649geYuEO_K_IM");
+const bot = new Eris("removed");
 
 const channelId = "574552831793692672"; //#ping-log
 
